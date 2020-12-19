1b4774f1f8899e5fcf7b185e882d7103b8057e86
Eric Jones <eric70x7@gmail.com>
2020-12-18 19:29:42 -0700

    Christmas lights and day/night heat schedule.

---
+++ python_scripts/nighttime_heat.py
@@ -0,0 +1,20 @@
+#
+# set the nighttime heat temp
+#
+
+# debug mode is default
+debug = int(data.get('debug', 1))
+
+temp = int(float(hass.states.get('input_number.nighttime_heat').state)) # 70
+
+# logger.info("Setting precool to {}.".format(precool_temp))
+
+hass.services.call('python_script','set_temp',{'location':'downstairs',
+                                               'setpoint':temp,
+                                               'set_heat':1,
+                                               'debug':debug})
+
+hass.services.call('python_script','set_temp',{'location':'upstairs',
+                                               'setpoint':temp,
+                                               'set_heat':1,
+                                               'debug':debug})
--- python_scripts/set_temp.py
+++ python_scripts/set_temp.py
@@ -8,6 +8,7 @@ debug = int(data.get('debug', 1))
 # name can be upstairs or downstairs
 location = data.get('location')
 setpoint = data.get('setpoint')
+set_heat = data.get('set_heat', 0)
 
 if location == 'upstairs':
     thermostat_id = 'climate.trane_corporation_model_tzemt524aa21ma_mode_2'
@@ -20,9 +21,18 @@ else:
 if not debug:
     # Get the current low setpoint
     low_setpoint=hass.states.get(thermostat_id).attributes['target_temp_low']
+    high_setpoint=hass.states.get(thermostat_id).attributes['target_temp_high']
+
+    if set_heat:
+        low_setpoint=setpoint
+        logger.info("Set heat to {}.".format(low_setpoint))
+    else:
+        high_setpoint=setpoint
+        logger.info("Set cool to {}.".format(high_setpoint))
+    
     # Make the adjustment
     hass.services.call('climate','set_temperature',{'entity_id':thermostat_id,
-                                                    'target_temp_high': setpoint,
+                                                    'target_temp_high': high_setpoint,
                                                     'target_temp_low': low_setpoint})
 else:
     logger.info("Debug mode. No change made.")
