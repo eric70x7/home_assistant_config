b9e8ee7d2346922f19a860628c77efc9e0af5d1b
Eric Jones <eajonese@pm.me>
2024-09-28 12:18:28 -0700

    Patio TV ceiling integration.  Comment out report_thermostats logging.

--- apps/report_thermostats.py
+++ apps/report_thermostats.py
@@ -1,5 +1,8 @@
 import appdaemon.plugins.hass.hassapi as hass
-
+""" 
+from datetime import datetime, timezone
+import pytz
+"""
 #
 # Report thermostats
 #
@@ -9,9 +12,20 @@ import appdaemon.plugins.hass.hassapi as hass
 class ReportThermostats(hass.Hass):
 
   def initialize(self):
-    # for some reason this gets called for either thermostat, though downstairs is specified
-    self.listen_state(self.therm_changed, entity='climate.downstairs_trane_thermostat', attribute='temperature')
+    # Listen for temperature changes on both thermostats
+    self.listen_state(self.therm_changed, entity_id='climate.downstairs_trane_thermostat', attribute='temperature')
+    self.listen_state(self.therm_changed, entity_id='climate.upstairs_trane_thermostat', attribute='temperature')
     self.log(f"ReportThermostats initialized")
 
   def therm_changed(self, entity, attribute, old, new, cb_args):
-    self.log(f"{entity} changed from {old} to {new}.")
+    # self.log(f"Entity:   {entity}:")
+    # self.log(f"Was {old}, changed to {new}.")
+
+    """ 
+    lr = datetime.fromisoformat(self.get_state(entity, attribute='last_reported'))
+    self.log(f"Last Reported: {lr.astimezone()}")
+    lc = datetime.fromisoformat(self.get_state(entity, attribute='last_changed'))
+    self.log(f"Last Changed:  {lc.astimezone()}")
+    lu = datetime.fromisoformat(self.get_state(entity, attribute='last_updated'))
+    self.log(f"Last Updated:  {lu.astimezone()}")
+    """
