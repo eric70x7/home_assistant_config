084261a5448529037fa87e9938b45636d3f692cc
Eric Jones <eajonese@pm.me>
2025-02-23 14:52:31 -0700

    Energy controller saves energy history, and doesn't use ureg.

--- apps/energy_controller.py
+++ apps/energy_controller.py
@@ -1,9 +1,10 @@
 import appdaemon.plugins.hass.hassapi as hass
-import math
-from pint import UnitRegistry
-from datetime import datetime, timedelta
+# import math
+# from pint import UnitRegistry
+# from datetime import datetime
+import json
 import numpy as np
-import pandas as pd
+# import pandas as pd
 """ 
 import pytz
 """
@@ -16,20 +17,28 @@ import pytz
 class EnergyController(hass.Hass):
 
   def initialize(self):
-    self.ureg = UnitRegistry()
-    self.Q_ = self.ureg.Quantity
-
+    ''' Constants '''
+    self.epoch=np.datetime64('1970-01-01')
+    self.time_unit=np.timedelta64(1,'s')
     self.energy_sensor='sensor.energy_usage'
+    self.energy_hist_name='app.energy_history'
 
     self.listen_state(self.energy_update, entity_id=self.energy_sensor)
-    self.log(f"EnergyController initialized")
 
-    self.last_energy_time=None
+    ''' list of (timestamp, energy) '''
+    self.te=[]
+    self.set_namespace("energy_namespace")
+    # to clear the state:
+    # self.set_state(self.energy_hist_name, state=json.dumps(self.te))
+    if self.get_state(self.energy_hist_name) != None:
+      txt = self.get_state(self.energy_hist_name)
+      self.te=[(self.time_unit*int(t),v) for t,v in json.loads(txt)]
+    self.set_namespace("default")
 
-    self.dt = []
-    self.p = []
+    self.ma_times=np.array([np.timedelta64(t,'m') for t in [10, 20, 30, 40, 50, 60]])
+    self.max_window=max(self.ma_times) + np.timedelta64(5, 'm')
 
-    self.max_window=1.0*self.ureg.hours + 5.0*self.ureg.minutes
+    # self.log(f"EnergyController initialized")
 
   '''
   Generic log for state listeners
@@ -37,38 +46,40 @@ class EnergyController(hass.Hass):
   def logit(self, entity, old, new):
     self.log(f"{entity}: Was {old}, changed to {new}.")
 
+  def time_since_epoch(self, t: np.datetime64):
+    return t-self.epoch
+
   '''
   Accumulate usage
   '''
   def energy_update(self, entity, attribute, old, new, cb_args):
-    t=datetime.now()
+    t1=self.time_since_epoch(np.datetime64('now'))
+    p=float(new) # watts
+    if self.te != []:
+      delta_t=t1-self.te[0][0]
+      e=p*delta_t/np.timedelta64(1,'h') # watt*hours
+      self.log(f"delta_t = {delta_t}, p = {p}, e = {e}")
+      
+      ''' Accumulate energy history '''
+      self.te.insert(0,(t1, 0))
+      self.te = [(t,en+e) for t,en in self.te if t1-t < self.max_window]
+      
+      ''' Save to namespace '''
+      self.set_namespace("energy_namespace")
+      save_hist = [(int(t/self.time_unit),v) for t,v in self.te]
+      self.set_state(self.energy_hist_name, state=json.dumps(save_hist))
+      self.set_namespace("default")
 
-    self.logit(entity, old, new)
+      ''' Interpolate at the desired moving average times '''
+      sample_times = np.array([t/self.time_unit for t,_ in self.te]) # convert absolute times to timedeltas
+      samples      = np.array([e for _,e in self.te])
+      ma_times     = np.array([(t1-t)/self.time_unit for t in self.ma_times])
+      ''' x must be increasing '''
+      interp_samples = np.flip(np.interp(np.flip(ma_times), np.flip(sample_times), np.flip(samples)))
 
-    p=float(new)*self.ureg.watt
-    if self.last_energy_time:
-      dt=(t-self.last_energy_time).total_seconds()*self.ureg.sec
-      e=p*dt
-      self.log(f"dt = {dt:.2f~#P}, p = {p:.2f~#P}, e = {e.to(self.ureg.watt_hour):.2f~#P}")
-      self.dt.insert(0,0*self.ureg.sec)
-      self.p.insert(0,0*self.ureg.watt)
-      '''
-      Accumulate
-      '''
-      self.dt = [dtn + dt for dtn in self.dt if dtn < self.max_window]
-      self.p  = [pn + p for pn in self.p][:len(self.dt)]
-      mt=[10, 20, 30, 40, 50, 60] * self.ureg.minute
+      self.log(f'{interp_samples}')
 
-      pddt=self.ureg.Quantity.from_list(self.dt)
-      pdp =self.ureg.Quantity.from_list(self.p)
-      pdmt=self.ureg.Quantity.from_list(mt)
-
-      s=np.interp(pdmt, pddt, pdp)
-
-      self.log(f"t: {mt}")
-      self.log(f"s: {s}")
-
-    df = pd.DataFrame(self.p, index=self.dt)
-    # self.log(f"df = {df}")
-
-    self.last_energy_time=t
+      ''' Update moving average sensors '''
+      for t,v in zip(self.ma_times,interp_samples):
+        u=v/1000 # kWh
+        self.set_state(f"sensor.ad_usage_{int(t/np.timedelta64(1,'m'))}", state=u, attributes={"unit_of_measurement": "kWh"})
--- apps/log_light.py
+++ apps/log_light.py
@@ -16,8 +16,16 @@ class LogLight(hass.Hass):
     self.log_light=self.get_entity(self.args["log_light_entity"])
     self.real_light=self.get_entity(self.args["real_light_entity"])
     self.log_light.listen_state(self.update_brightness, attribute = "brightness")
-    self.log_light.listen_state(self.update_state, attribute = "state")
+    self.log_light.listen_state(self.update_real_state, attribute = "state")
+    self.real_light.listen_state(self.update_log_state, attribute = "state")
+    # self.real_light.listen_state(self.update_log_state, attribute = "state")
     self.range=255
+    
+    '''Sync the initial state according to the real light current state'''
+    if self.real_light.get_state() == "on":
+      self.log_light.call_service("turn_on")
+    else:
+      self.log_light.call_service("turn_off")
 
   '''
   Generic log for state listeners
@@ -26,14 +34,24 @@ class LogLight(hass.Hass):
     self.log(f"{entity}[{attribute}]: Was {old}, changed to {new}.")
 
   '''
-  Turn on/off
+  Keep the states in sync
   '''
-  def update_state(self, entity, attribute, old, new, cb_args):
-    # self.logit(entity, attribute, old, new)
-    if new == "off":
-      self.real_light.call_service("turn_off")
-    else:
-      self.real_light.call_service("turn_on")
+  def update_real_state(self, entity, attribute, old, new, cb_args):
+    if not new == self.real_light.get_state():
+      if new == "off":
+        self.real_light.call_service("turn_off")
+      else:
+        self.real_light.call_service("turn_on")
+    
+  '''
+  Keep the states in sync
+  '''
+  def update_log_state(self, entity, attribute, old, new, cb_args):
+    if not new == self.log_light.get_state():
+      if new == "off":
+        self.log_light.call_service("turn_off")
+      else:
+        self.log_light.call_service("turn_on")
     
   '''
   Adjust the brightness
